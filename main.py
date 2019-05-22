e9953fb1f363155b422c4102c9b1a39b19af0fb6
Alexander <alex20k.x@gmail.com>
2019-05-22 22:14:12 +0600

    Code optimization

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_security import SQLAlchemyUserDatastore
 
 from forms import regforms
+from models import User, Role
+from configurationFile import ConfigClass
 import requests
 
 app = Flask(__name__)
-
-from configurationFile import ConfigClass
 
 app.config.from_object(ConfigClass)
 
@@ -17,22 +17,15 @@ db = SQLAlchemy(app)
 # python -m smtpd -n -c DebuggingServer localhost:8025 - Emulated mail server
 mail = Mail(app)
 
-
-from models import User, Role
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def define_welcome():
 
-    url = ('https://newsapi.org/v2/top-headlines?'
-            'q=game of thrones&'
-            'apiKey=397dc499222b4d158971b8cb46f1fa4b')
+    url = 'https://newsapi.org/v2/top-headlines?q=game of thrones&apiKey=397dc499222b4d158971b8cb46f1fa4b'
 
-    try:
-        content = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        err = 'You are not connected to the Internet'
-        return render_template('page.html', err=err)
+    content = requests.get(url)
 
     data = content.json()
     data_articles = data['articles']
@@ -54,17 +47,18 @@ def define_register():
 
         mail.send(msg)
 
-        #new_user = user_datastore.create_user(name=nameuser, email=emailuser, password=passworduser)
+        # new_user = user_datastore.create_user(name=nameuser, email=emailuser, password=passworduser)
 
-        #db.session.add(new_user)
-        #db.session.commit()
+        # db.session.add(new_user)
+        # db.session.commit()
 
     return render_template('register.html', regforms=regforms)
 
 
 @app.route('/signin', methods=['GET', 'POST'])
 def define_login():
-     pass
+    pass
+
 
 if __name__ == "__main__":
     app.run()
