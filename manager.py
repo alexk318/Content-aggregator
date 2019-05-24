fe29033b14f6fc00addd9902e7ebcd82dfc0a90d
Alexander <alex20k.x@gmail.com>
2019-05-24 18:38:50 +0600

    'migrate' was needed

--- manager.py
+++ manager.py
@@ -1,11 +1,11 @@
-from main import app
+from configurationFile import app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+migrate = Migrate(app, db)
+
 manager = Manager(app)
 manager.add_command('dbcommand', MigrateCommand)
-
-# migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     manager.run()
