32269f574f11f63ab19afd1904facaf1d7367234
Alexander <alex20k.x@gmail.com>
2019-07-09 20:33:07 +0600

    Project structure changed

---
+++ config.py
@@ -0,0 +1,20 @@
+import mysql.connector
+import os
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = 'SECRETKEY'
+
+SECURITY_PASSWORD_SALT = 'email-confirm'
+SECURITY_PASSWORD_HASH = 'sha512_crypt'
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:microlabm666@localhost/aggregatordb'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'alex20k.x@gmail.com'
+MAIL_PASSWORD = 'microlabM666'
+
+del os
